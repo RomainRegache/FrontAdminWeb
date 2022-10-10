6d08b901df0da026a2676164902c869194a9ffbe
Maxence Draud <maxence.draud@thatsitstudio.fr>
2022-10-10 10:43:28 +0200

    Add backend initial code

---
+++ Back/projetadminweb-back/src/main/java/com/example/projetadminwebback/ProjetadminwebBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetadminwebback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetadminwebBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetadminwebBackApplication.class, args);
+    }
+
+}
---
+++ Back/projetadminweb-back/src/test/java/com/example/projetadminwebback/ProjetadminwebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetadminwebback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetadminwebBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
